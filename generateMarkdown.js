9f1c8025fdee6bc156ade6c7def1c7025ad29410
Jeff McMillen <jeff.mcmillen@playableworlds.com>
2023-07-23 18:29:26 -0500

    Created renderLicenseBadge function to display badge near the top of the README

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,6 +1,52 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const license = data.license;
+  let licenseBadge = "";
+  switch (license) {
+    case 'Apache License 2.0':
+      licenseBadge = "https://img.shields.io/badge/License-Apache_2.0-blue.svg";
+      break;
+    case 'GNU General Public License v3.0':
+      licenseBadge = "https://img.shields.io/badge/License-GPLv3-blue.svg";
+      break;
+    case 'MIT License':
+      licenseBadge = "https://img.shields.io/badge/License-MIT-yellow.svg";
+      break;
+    case 'BSD 2-Clause "Simplified" License':
+      licenseBadge = "https://img.shields.io/badge/License-BSD_2--Clause-orange.svg";
+      break;
+    case 'BSD 3-Clause "New/Revised" License':
+      licenseBadge = "https://img.shields.io/badge/License-BSD_3--Clause-blue.svg";
+      break;
+    case 'Boost Software License 1.0':
+      licenseBadge = "https://img.shields.io/badge/License-Boost_1.0-lightblue.svg";
+      break;
+    case 'Creative Commons Zero v1.0 Universal':
+      licenseBadge = "https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg";
+      break;
+    case 'Eclipse Public License 1.0':
+      licenseBadge = "https://img.shields.io/badge/License-EPL_1.0-red.svg";
+      break;
+    case 'GNU Affero General Public License v3.0':
+      licenseBadge = "https://img.shields.io/badge/License-AGPL_v3-blue.svg";
+      break;
+    case 'GNU General Public License v2.0':
+      licenseBadge = "https://img.shields.io/badge/License-GPL_v2-blue.svg";
+      break;
+    case 'GNU Lesser General Public License v3.0':
+      licenseBadge = "https://img.shields.io/badge/License-LGPL_v3-blue.svg";
+      break;
+    case 'Mozilla Public License 2.0':
+      licenseBadge = "https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg";
+      break;
+    case 'The Unlicense':
+      licenseBadge = "https://img.shields.io/badge/license-Unlicense-blue.svg";
+      break;
+    default:
+      break;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -38,6 +84,9 @@ function generateMarkdown(data) {
   ${data.credits}
   
   ## License
+
+  This application uses the following license: 
+  ${data.license}
 
   ## Contributing
 
--- index.js
+++ index.js
@@ -33,7 +33,7 @@ inquirer
         type: 'list',
         message: 'Choose the license for your project.',
         name: 'license',
-        choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New/Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense'],
+        choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New/Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 1.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v3.0', 'Mozilla Public License 2.0', 'The Unlicense'],
     },
   ]);
 
