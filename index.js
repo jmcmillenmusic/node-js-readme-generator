130230d9c81e9c6f5bcdccad8b7cd9ebd803218c
Jeff McMillen <jeff.mcmillen@playableworlds.com>
2023-07-23 22:55:20 -0500

    Continued working on the script for the questions

--- index.js
+++ index.js
@@ -35,6 +35,16 @@ inquirer
         name: 'license',
         choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New/Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 1.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v3.0', 'Mozilla Public License 2.0', 'The Unlicense'],
     },
+    {
+      type: 'input',
+      message: 'What contribution guidelines would you like to set up for your project?',
+      name: 'contribution',
+    },
+    {
+      type: 'input',
+      message: 'What test instructions would you like to set up for your project?',
+      name: 'test',
+    },
   ]);
 
 // TODO: Create a function to write README file
